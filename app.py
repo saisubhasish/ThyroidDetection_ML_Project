85ce0249bd96fab64c23ef445f837af2bd2a04a9
Sai <saisubhasishrout777@gmail.com>
2023-01-31 05:37:54 +0530

    added app.py and home.html

---
+++ app.py
@@ -0,0 +1,44 @@
+import sys
+from flask import Flask, request, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+from thyroid.predictor import ModelResolver
+from thyroid.logger import logging
+from thyroid.utils import load_object
+from thyroid.exception import ThyroidException
+
+app = Flask(__name__)
+
+
+logging.info("Creating model resolver object")
+model_resolver = ModelResolver(model_registry="saved_models")   # Location where models are saved
+
+
+# Load the model
+target_encoder = load_object(file_path=model_resolver.get_latest_target_encoder_path())
+model = load_object(file_path=model_resolver.get_latest_model_path())
+
+@app.route('/')
+def home():
+    try:
+        return render_template('home.html')
+    
+    except Exception as e:
+        raise ThyroidException(error_message=e, error_detail=sys)
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    try:
+        data = [float(x) for x in request.form.values()]
+        prediction = model.predict(data)
+        cat_prediction = target_encoder.inverse_transform(prediction)
+        print(cat_prediction)
+        
+        return render_template('home.html', output_text="The Quality of Wine is {}.".format(cat_prediction))
+    
+    except Exception as e:
+        raise ThyroidException(error_message=e, error_detail=sys)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'))
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from thyroid.exception import ThyroidException
 from thyroid.pipeline.training_pipeline import start_training_pipeline
 from thyroid.pipeline.batch_prediction import start_batch_prediction
 
-input_file_path= "/config/workspace/hypothyroid.csv"
+input_file_path= "D:/FSDS-iNeuron/10.Projects-DS/ThyroidDetection_ML_Project/hypothyroid.csv"
 
 
 if __name__ == "__main__":
      try:
-         start_training_pipeline()
-         #output_file = start_batch_prediction(input_file_path=input_file_path)
-         #print(output_file)
+         #start_training_pipeline()
+         output_file = start_batch_prediction(input_file_path=input_file_path)
+         print(output_file)
 
      except Exception as e:
           raise ThyroidException(error_message=e, error_detail=sys)
